e5216777eb11a736b72ec74a5a0e7e5e84c53098
HydoWave <the.vodik.man@gmail.com>
2024-11-28 12:49:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-window.addEventListener("scroll", () => {
-    const scrollThreshold = window.innerHeight / 2;
-    const body = document.body;
+// Get the modal
+var modal = document.getElementById("modal");
 
-    if (window.scrollY > scrollThreshold) {
-        body.classList.add("scrolled");
-    } else {
-        body.classList.remove("scrolled");
+// Get the image element inside the modal
+var modalImg = document.getElementById("modal-image");
+
+// Get the close button
+var closeButton = document.querySelector(".close-button");
+
+// Function to open the modal with the clicked image
+document.querySelectorAll(".product-image").forEach(function(img) {
+    img.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalImg.alt = this.alt;
     }
 });
+
+// Close the modal when the close button is clicked
+closeButton.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Close the modal when clicking outside of the modal content
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
